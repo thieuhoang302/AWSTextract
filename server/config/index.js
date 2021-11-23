5315d3908caa57c8aec5d2e192b9efb224db933e
ThieuVanHoang <thieuvanhoang.0015@gmail.com>
2021-11-24 01:12:20 +0700

    Fix config

--- server/config/index.js
+++ server/config/index.js
@@ -5,6 +5,18 @@ const config = {
   secrets: {
     JWT_SECRET: process.env.JWTSECRET
   },
+  keys: {
+    s3: {
+      accessKeyId: process.env.S3ACCESSKEYID,
+      secretAccessKey: process.env.S3SECRETKEY,
+      bucketName: process.env.S3BUCKET
+    },
+    ses: {
+      sesRegion: process.env.SESREGION,
+      accessKeyId: process.env.SESACCESSKEYID,
+      secretAccessKey: process.env.SESSECRETKEY
+    }
+  },
   port: process.env.PORT
 }
 
